cb99d31e791cc51862219380036db389944d7b87
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-05-06 11:39:32 +0300

    Clean up code

--- model_reviews/signals.py
+++ model_reviews/signals.py
@@ -7,14 +7,12 @@ from model_reviews.models import AbstractReview, ModelReview
 
 
 @receiver(post_save)
-def after_save(  # pylint: disable=bad-continuation
+def approvable_after_save(  # pylint: disable=bad-continuation
     sender, instance, raw, created, **kwargs
 ):  # pylint: disable=unused-argument
     """Manage data in the approvable item after it has been."""
     if isinstance(instance, AbstractReview) and not isinstance(instance, ModelReview):
         if created:
             obj_type = ContentType.objects.get_for_model(instance)
-
             review = ModelReview(content_type=obj_type, object_id=instance.id)
-
             review.save()

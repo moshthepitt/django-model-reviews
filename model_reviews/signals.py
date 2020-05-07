13e0a5ba8c052fd10e7a664b90ed1402d30afb34
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-05-07 11:42:43 +0300

    Add modelreview_after_save

--- model_reviews/signals.py
+++ model_reviews/signals.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
 from model_reviews.models import AbstractReview, ModelReview
+from model_reviews.utils import process_review
 
 
 @receiver(post_save)
@@ -16,3 +17,11 @@ def approvable_after_save(  # pylint: disable=bad-continuation
             obj_type = ContentType.objects.get_for_model(instance)
             review = ModelReview(content_type=obj_type, object_id=instance.id)
             review.save()
+
+
+@receiver(post_save, sender=ModelReview)
+def modelreview_after_save(  # pylint: disable=bad-continuation
+    sender, instance, raw, created, **kwargs
+):  # pylint: disable=unused-argument
+    """Manage data in the approvable item after it has been."""
+    process_review(instance)

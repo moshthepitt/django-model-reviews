375bfcd915c4c84b184ee25708669ac06db1064c
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-05-19 22:46:55 +0300

    Add save and clean methods to PerformReview

--- model_reviews/forms.py
+++ model_reviews/forms.py
@@ -1,6 +1,8 @@
 """forms module for model_reviews"""
 from django import forms
 from django.conf import settings
+from django.db import transaction
+from django.utils import timezone
 from django.utils.translation import gettext as _
 
 from model_reviews.models import ModelReview, Reviewer
@@ -35,7 +37,9 @@ class PerformReview(forms.Form):
         """Initialize the form."""
         super().__init__(*args, **kwargs)
         self.request = kwargs.pop("request", None)
-        self.vega_extra_kwargs = kwargs.pop(settings.VEGA_MODELFORM_KWARG, dict())
+        self.vega_extra_kwargs = kwargs.pop(
+            getattr(settings, "VEGA_MODELFORM_KWARG", "vega_extra_kwargs"), dict()
+        )
 
     def clean_review_status(self):
         """Clean review_status."""
@@ -45,3 +49,30 @@ class PerformReview(forms.Form):
                 settings.MODELREVIEW_FORM_REVIEW_STATUS_VALIDATION_MSG
             )
         return data
+
+    def clean(self):
+        """Clean the entire form."""
+        cleaned_data = super().clean()
+        review_status = cleaned_data.get("review_status")
+        if review_status == ModelReview.REJECTED:
+            review_reason = cleaned_data.get("review_reason")
+            if not review_reason:
+                msg = _(settings.MODELREVIEW_FORM_REVIEW_MISSING_REASON)
+                self.add_error("review_reason", msg)
+
+    def save(self):
+        """Save the form."""
+        data = self.cleaned_data
+        review = data["review"]
+        reviewer = data["reviewer"]
+        now = timezone.now()
+        with transaction.atomic():
+            # save reviewer stuff
+            reviewer.reviewed = True
+            reviewer.review_date = now
+            reviewer.save()
+            # save review
+            review.review_status = data["review_status"]
+            review.review_reason = data.get("review_reason", "")
+            review.review_comments = data.get("review_comments", "")
+            review.save()

e9a7cb1dd4c6f76c6b2d3e3141c85fb428d29c89
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-05-29 22:33:57 +0300

    Add email strings

--- model_reviews/constants.py
+++ model_reviews/constants.py
@@ -1,9 +1,11 @@
 """Constants module."""
 USER = "user"
 SANDBOX_FIELD = "_sandbox"
-REVIEW_REQUEST_EMAIL_TXT = ""
-REVIEW_REQUEST_EMAIL_SUBJ = ""
-REVIEW_COMPLETE_EMAIL_TXT = ""
-REVIEW_COMPLETE_EMAIL_SUBJ = ""
+REVIEW_REQUEST_EMAIL_TXT = "There has been a new request that needs your attention."
+REVIEW_REQUEST_EMAIL_SUBJ = "New Request For Approval"
+REVIEW_COMPLETE_EMAIL_TXT = (
+    "Your request has been processed, please log in to view the status."
+)
+REVIEW_COMPLETE_EMAIL_SUBJ = "Your request has been processed"
 EMAIL_TEMPLATE = "generic"
 EMAIL_TEMPLATE_PATH = "model_reviews/email"

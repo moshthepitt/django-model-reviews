17a0ab20b2ad6dc67f023f2c90a82eae05de1a69
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-05-30 19:04:00 +0300

    Add form validation messages

--- model_reviews/constants.py
+++ model_reviews/constants.py
@@ -9,3 +9,5 @@ REVIEW_COMPLETE_EMAIL_TXT = (
 REVIEW_COMPLETE_EMAIL_SUBJ = "Your request has been processed"
 EMAIL_TEMPLATE = "generic"
 EMAIL_TEMPLATE_PATH = "model_reviews/email"
+REVIEW_FORM_SUCCESS_MSG = "Saved successfully. :)"
+REVIEW_FORM_FAIL_MSG = "Please correct the errors on the form."

dfc8e90272e11d65b5c7d63650d3ca6cd4f22fb2
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-05-30 09:41:21 +0300

    Add ReviewView

--- model_reviews/views.py
+++ model_reviews/views.py
@@ -1,1 +1,58 @@
 """Views module for model reviews."""
+from django.views import View
+from django.views.generic import DetailView, FormView
+from django.views.generic.detail import SingleObjectMixin
+
+from model_reviews.forms import get_review_form
+from model_reviews.models import ModelReview
+
+
+class ReviewFormMixin:  # pylint: disable=too-few-public-methods
+    """Mixin that implements a method to get review form."""
+
+    model = ModelReview
+
+    def get_form_class(self):
+        """Return the form class to use."""
+        return get_review_form(review=self.get_object(), user=self.request.user)
+
+
+class ReviewDisplay(ReviewFormMixin, DetailView):
+    """Detailview for a model review object."""
+
+    def get_context_data(self, **kwargs):
+        """Get context data."""
+        context = super().get_context_data(**kwargs)
+        form_class = self.get_form_class()
+        context["form"] = form_class()
+        return context
+
+
+class ReviewForm(ReviewFormMixin, SingleObjectMixin, FormView):
+    """FormView for a review."""
+
+    def get_success_url(self):
+        """Get the success url."""
+        return "/"
+
+
+class ReviewView(View):
+    """
+    View used to display details of a ModelReview object
+
+    This view shows both the model review object details as well as handles the
+    form to perform a review.
+
+    The structure is inspired by:
+        https://docs.djangoproject.com/en/3.0/topics/class-based-views/mixins/#an-alternative-better-solution
+    """
+
+    def get(self, request, *args, **kwargs):
+        """Get the ReviewDisplay view."""
+        view = ReviewDisplay.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """Get the ReviewForm view."""
+        view = ReviewForm.as_view()
+        return view(request, *args, **kwargs)

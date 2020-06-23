1727e349a29f2c486780796c4f853172ed710f9e
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-23 22:09:32 +0300

    ↑ bump to version 1.0.2

--- model_reviews/__init__.py
+++ model_reviews/__init__.py
@@ -1,5 +1,5 @@
 """Main init file for django-model-reviews."""
-VERSION = (1, 0, 1)
+VERSION = (1, 0, 2)
 __version__ = ".".join(str(v) for v in VERSION)
 # pylint: disable=invalid-name
 default_app_config = "model_reviews.apps.ModelReviewsConfig"  # noqa

c36b0b7b4c80083ef3f2085ee9a9c64816b76c52
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-10-03 22:27:23 +0300

    Add BulkReviewsView

--- model_reviews/urls.py
+++ model_reviews/urls.py
@@ -1,10 +1,11 @@
 """urls module."""
 from django.urls import path
 
-from model_reviews.views import ReviewView
+from model_reviews.views import BulkReviewsView, ReviewView
 
 app_name = "partners"
 
 urlpatterns = [
     path("review/<int:pk>", ReviewView.as_view(), name="perform_review"),
+    path("bulk-review", BulkReviewsView.as_view(), name="perform_bulk_review"),
 ]
--- model_reviews/views.py
+++ model_reviews/views.py
@@ -1,13 +1,23 @@
 """Views module for model reviews."""
+from typing import List, Optional
+
+from django.http import HttpResponseRedirect
 from django.utils.translation import gettext_lazy as _
 from django.views import View
 from django.views.generic import DetailView, FormView
+from django.views.generic.base import TemplateView
 from django.views.generic.detail import SingleObjectMixin
 
-from braces.views import FormInvalidMessageMixin, FormValidMessageMixin
+from braces.views import FormInvalidMessageMixin, FormValidMessageMixin, MessageMixin
 
-from model_reviews.constants import REVIEW_FORM_FAIL_MSG, REVIEW_FORM_SUCCESS_MSG
+from model_reviews.constants import (
+    REVIEW_FORM_FAIL_MSG,
+    REVIEW_FORM_SUCCESS_MSG,
+    REVIEW_FORMSET_FAIL_MSG,
+    REVIEW_FORMSET_SUCCESS_MSG,
+)
 from model_reviews.forms import get_review_form
+from model_reviews.formset import get_review_formset
 from model_reviews.models import ModelReview
 
 
@@ -82,3 +92,85 @@ class ReviewView(View):
         """Get the ReviewForm view."""
         view = ReviewForm.as_view()
         return view(request, *args, **kwargs)
+
+
+class BulkReviewsView(MessageMixin, TemplateView):
+    """View used to handle bulk reviews."""
+
+    initial: List[dict] = []
+    success_url: Optional[str] = None
+    prefix: Optional[str] = None
+    template_name = "model_reviews/bulk.html"
+    formset_invalid_message = _(REVIEW_FORMSET_FAIL_MSG)
+    formset_valid_message = _(REVIEW_FORMSET_SUCCESS_MSG)
+
+    def get_initial(self):
+        """Return a copy of the initial data to use for formsets on this view."""
+        return self.initial[:]
+
+    def get_prefix(self):
+        """Return the prefix to use for forms."""
+        return self.prefix
+
+    def get_formset_class(self):
+        """Get formset."""
+        return get_review_formset(user=self.request.user)
+
+    def get_formset(self, formset_class=None):
+        """Return an instance of the formset to be used in this view."""
+        if formset_class is None:
+            formset_class = self.get_formset_class()
+        return formset_class(**self.get_formset_kwargs())
+
+    def get_formset_kwargs(self):
+        """Return the keyword arguments for instantiating the formset."""
+        kwargs = {
+            "initial": self.get_initial(),
+            "prefix": self.get_prefix(),
+        }
+
+        if self.request.method in ("POST", "PUT"):
+            kwargs.update(
+                {
+                    "data": self.request.POST,
+                    "files": self.request.FILES,
+                }  # flake8: noqa
+            )
+        return kwargs
+
+    def get_success_url(self):
+        """Return the URL to redirect to after processing a valid form."""
+        if not self.success_url:
+            return self.request.get_full_path()
+        return str(self.success_url)  # success_url may be lazy
+
+    def get_context_data(self, **kwargs):
+        """Insert the formset into the context dict."""
+        if "formset" not in kwargs:
+            kwargs["formset"] = self.get_formset()
+        return super().get_context_data(**kwargs)
+
+    def formset_valid(self, formset):  # pylint: disable=unused-argument
+        """If the form is valid, redirect to the supplied URL."""
+        for form in formset:
+            if form.has_changed():
+                form.save()
+        self.messages.success(self.formset_valid_message, fail_silently=True)
+        return HttpResponseRedirect(self.get_success_url())
+
+    def formset_invalid(self, formset):
+        """If the form is invalid, render the invalid form."""
+        self.messages.error(self.formset_invalid_message, fail_silently=True)
+        return self.render_to_response(self.get_context_data(formset=formset))
+
+    def post(self, request, *args, **kwargs):  # pylint: disable=unused-argument
+        """
+        Handle POST requests.
+
+        Instantiate a form instance with the passed POST variables and then
+        check if it's valid.
+        """
+        formset = self.get_formset()
+        if formset.is_valid():
+            return self.formset_valid(formset)
+        return self.formset_invalid(formset)
